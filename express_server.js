d78c3cdc96b2c192512b6389b8a10f6b00ef2009
Sputn1kolas <nikolas.clark@mail.mcgill.ca>
2017-07-05 19:40:17 +0000

    logout, all features working. Cannot get logout button on right line

--- express_server.js
+++ express_server.js
@@ -7,13 +7,14 @@ const app = express();
 app.set("view engine", "ejs");
 const port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser())
 
 //Pseudo-Database
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 function generateRandomString() {
   var string = "";
@@ -31,21 +32,23 @@ function generateRandomString() {
 //   res.end("I AM THE ROOT WEBPAGE, HELLOOO");
 // });
 
+
 app.get("/urls", function(req, res){
-  let templateVar = {urls: urlDatabase};
+  let templateVar = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("../urls_index", templateVar);
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVar = {urls: urlDatabase};
+  let templateVar = {urls: urlDatabase, username: req.cookies["username"]};
   res.render("../urls_new",templateVar);
 });
 
 app.get("/urls/:id", function(req, res) {
-  let templateVar = {
-    shortURL: req.params.id,
-    urls: urlDatabase
-  };
+  let templateVar = {urls: urlDatabase, username: req.cookies["username"]};
+  templateVar.shortURL = req.params.id,
   res.render("../urls_show", templateVar);
 });
 
@@ -74,7 +77,22 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 
+// Login + posts cookie
+app.post("/login", (req, res) => {
+  console.log(req.body["username"])
+  res.cookie('username', req.body["username"], { maxAge: 900000})
+  res.redirect("/urls")
+});
+
+//logout
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect("/urls")
+  console.log("logged out!")
+});
+
+
 // get app to listen on port 8080
 app.listen(port, function(){
-  console.log(`Hello, this is me, your express server, I'm listening... (on port ${port})`)
+  console.log(`Good News Everyone!, I'm listening... on port ${port}`)
 })
